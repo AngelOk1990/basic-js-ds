6b8e724fb9abe69fdbc35b77d1863179e4dbdf8e
AngelOk1990 <pandafree@mail.ru>
2022-05-02 16:25:08 +0300

    feat: make task tack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(...args) {
+    this.store = [...args.reverse()];
+  }
+  push(element) {
+    return this.store.unshift(element)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.store.shift();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.store[0]
   }
 }
 
